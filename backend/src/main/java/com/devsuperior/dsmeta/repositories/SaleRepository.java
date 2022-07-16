6778b97f3984adbeaaf970610393fc6dcb627af6
Victor Oliveira <vimatozin@hotmail.com>
2022-07-16 13:15:59 -0300

    second part

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsmeta.repositories;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface saleRepository extends JpaRepository<Sale,Long>{
+}
