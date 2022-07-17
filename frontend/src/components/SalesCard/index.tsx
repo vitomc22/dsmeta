1e0ab17aa3e8b21c2c07775e9c3114fc84a6e34a
Victor Oliveira <vimatozin@hotmail.com>
2022-07-17 12:41:34 -0300

    Fixed deals

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -73,7 +73,7 @@ function SalesCard() {
                                         <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
                                         <td>{sale.sellerName}</td>
                                         <td className="show992">{sale.visited}</td>
-                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
                                         <td>R$ {sale.amount.toFixed(2)}</td>
                                         <td>
                                             <div className="dsmeta-red-btn-container">
