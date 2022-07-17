c77728b2df5a50e0e47d3ac8c2bbe7ca34cdd119
Victor Oliveira <vimatozin@hotmail.com>
2022-07-16 23:43:20 -0300

    Send notification

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -27,7 +27,7 @@ public class SmsService {
         Sale sale = saleRepository.findById(saleid).get();
         String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
-        String msg = " O vendedor" + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ " + String.format("%.2f", sale.getAmount());
+        String msg = " O vendedor " + sale.getSellerName() + " foi destaque em " + date + " com um total de R$ " + String.format("%.2f", sale.getAmount());
 
         Twilio.init(twilioSid, twilioKey);
 
