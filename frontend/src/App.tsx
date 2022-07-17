3a03f41f295ff47226cad791c315a33278dd333b
Victor Oliveira <vimatozin@hotmail.com>
2022-07-16 23:49:39 -0300

    Send notification TOAST

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import SalesCard from "./components/SalesCard"
 import Header from "./components/Header"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
     return (
         <>
+        <ToastContainer/>
             <Header/>
             <main>
                 <section id="sales">
